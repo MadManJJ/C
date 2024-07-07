a691ab1d9e184bc2d9147356faa9e05faaca9c93
MadManJJ <123284870+MadManJJ@users.noreply.github.com>
2024-07-07 16:42:44 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
-// const person = {
-//     name: "John",
-//     age: 30,
-//     city: "New York"
-//   };
-//   const jsonString = JSON.stringify(person);
-//   console.log(jsonString); // Outputs: '{"name":"John","age":30,"city":"New York"}'
-const jsonpure = '{"name":"John","age":30,"city":"New York"}';
-const jsonJSobject = JSON.parse(jsonpure); //parse a JSON to a javascript object
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  const jsonString = JSON.stringify(person); //change a JS object into a JSON(string)
+  console.log(jsonString); // Outputs: '{"name":"John","age":30,"city":"New York"}'
+
+
+const jsonpure = '{"name":"John","age":30,"city":"New York"}'; //this is a JSON(string)
+const jsonJSobject = JSON.parse(jsonpure); //parse a JSON to a javascript object(jsonJSobject is a JS object from jsonpure(JSON string))
 console.log(jsonJSobject);
 console.log(jsonJSobject.age);
 
